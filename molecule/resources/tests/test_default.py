1fa4b9fc18f2b5fabfd7093148ea3254339694e5
khaledk2 <khaledk2@hotmail.com>
2025-11-25 16:37:24 +0000

    Fix issues and clean up the code

--- molecule/resources/tests/test_default.py
+++ molecule/resources/tests/test_default.py
@@ -6,6 +6,30 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-def test_nginx_gateway(host):
-    out = host.check_output('curl -L localhost')
-    assert 'OMERO.web - Login' in out
+def test_elasticsearch_cluster(host):
+    out = host.check_output(
+        'curl -I  -k -u "elastic:my_password" https://127.0.0.1:9201/')
+    assert '200' in out
+
+def test_search_elastic_connection(host):
+    out = host.check_output(
+        'curl -I  -k -u "elastic:my_password" https://127.0.0.1:9201/image_keyvalue_pair_metadata_1')
+    assert '200' in out
+
+#https://127.0.0.1:9201/image_keyvalue_pair_metadata_1
+#def test_nginx_gateway(host):
+#    out = host.check_output('curl -XGET http://127.0.0.1:8080/searchengine/api/v1/resources/')
+#    assert 'OMERO search engine (API V1)' in out
+
+#def test_redis_connection(host):
+#    out = host.check_output('redis-cli ping')
+#    assert 'PONG' in out
+
+def test_searchengine_connection(host):
+    out = host.check_output('curl -XGET http://127.0.0.1:5577/searchengine/api/v1/resources/')
+    assert 'OMERO search engine (API V1)' in out
+
+def test_searchengine_connection(host):
+    out = host.check_output('curl -XGET http://127.0.0.1:5577/searchengine/api/v1/resources/')
+    assert 'OMERO search engine (API V1)' in out
+
